0eb2463fd23fca18d61f168f8bc663c7308c11c4
katierose9 <kwhitney98@gmail.com>
2018-01-16 16:12:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function dec(a) {
   return a-=1
 }
 function makeInt(n) {
-  return parseInt('2', 10)
+  return parseInt('n', 10)
 }
 function preserveDecimal(n) {
-  return parseFloat('2.2222')
+  return parseFloat('n')
 }
